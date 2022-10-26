48e113f6ff816a0fa44d4b263be0d83aa7b57e25
olisah mbaeyi <olisah0882@gmail.com>
2022-10-25 21:08:36 -0400

    fix syntax issue

--- JavaWebApp/src/main/java/com/example/demo/DemoApplication.java
+++ JavaWebApp/src/main/java/com/example/demo/DemoApplication.java
@@ -9,10 +9,9 @@ public class DemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 
-		string name = "mike";
+		String name = "vamsi";
 
-		system.out.println(name);
-
+		System.out.println(name);
 	}
 
 }
